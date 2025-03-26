b41e214b51d5c9d70e926117a4b8b10ce2aae9f2
androidxq <nopainsnogainslf@gmail.com>
2025-03-26 08:45:39 +0800

    k线展示部分，数据动态加载出现问题，目前难以修复

---
+++ config/api_config.py
@@ -0,0 +1,6 @@
+# 币安API配置
+BINANCE_CONFIG = {
+    "api_key": "uMfetP97rZhrkuae7T3QUOdvvqttjMbgXYSscGAZg2xEDjQOPgB5u71qh7AVQ1Lm",
+    "api_secret": "WTpv1LN7cFCNqN0wGuDLRghfGj4jUiLm1Fq2SrtGag96BdAAfrsxM2RYd5UXN5vx",
+    "proxy": "http://127.0.0.1:7890"  # 添加代理配置，端口号根据实际情况修改
+} 
